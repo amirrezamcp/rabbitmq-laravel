0aa883a53d12df83a5d68efc10c2660eb28e31e7
“Amirrezamcp” <mousavi.amirreza30@gmail.com>
2026-06-04 11:34:54 +0330

    Create Product Model & Migrate

---
+++ admin/app/Models/Product.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+#[Fillable(['title', 'image', 'likes'])]
+class Product extends Model
+{
+    use HasFactory;
+}
