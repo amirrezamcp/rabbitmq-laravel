2dc8062ad8db96cc45cd80ad6ddb0cb3e726f1cd
“Amirrezamcp” <mousavi.amirreza30@gmail.com>
2026-06-04 11:44:28 +0330

    Update Model [ admin ]

--- admin/app/Models/Product.php
+++ admin/app/Models/Product.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-#[Fillable(['title', 'image', 'likes'])]
+#[Fillable(['title', 'image'])]
 class Product extends Model
 {
     use HasFactory;
