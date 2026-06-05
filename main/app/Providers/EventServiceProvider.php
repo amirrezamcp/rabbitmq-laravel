c8a8c0bf804136823880770496274835ab04bf5c
“Amirrezamcp” <mousavi.amirreza30@gmail.com>
2026-06-05 12:59:22 +0330

    Update Event Service Provuder

--- main/app/Providers/EventServiceProvider.php
+++ main/app/Providers/EventServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use App\Jobs\ProductCreated;
+use App\Jobs\ProductDeleted;
+use App\Jobs\ProductUpdated;
 use Illuminate\Support\ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
@@ -10,5 +12,7 @@ class EventServiceProvider extends ServiceProvider
     public function boot(): void
     {
         \app()->bindMethod(ProductCreated::class . '@handle', fn($job) => $job->handle());
+        \app()->bindMethod(ProductUpdated::class . '@handle', fn($job) => $job->handle());
+        \app()->bindMethod(ProductDeleted::class . '@handle', fn($job) => $job->handle());
     }
 }
