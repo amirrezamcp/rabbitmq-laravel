deefdc5c4ca1485ccb3319042b7e38e3be8a00d7
“Amirrezamcp” <mousavi.amirreza30@gmail.com>
2026-06-05 11:58:34 +0330

    Update Model ( fix bug )

--- main/app/Models/Product.php
+++ main/app/Models/Product.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 
+#[Fillable(['id', 'title', 'image'])]
 class Product extends Model
 {
     //
