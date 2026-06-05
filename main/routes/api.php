3303f6617509c1d6d98fb66048e9e20c1a1a831a
“Amirrezamcp” <mousavi.amirreza30@gmail.com>
2026-06-05 12:59:29 +0330

    fix big

--- main/routes/api.php
+++ main/routes/api.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource("Product", ProductController::class);
+Route::apiResource('products', ProductController::class);
