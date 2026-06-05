c1e091764e71786c38818c3db14c44442e24e189
“Amirrezamcp” <mousavi.amirreza30@gmail.com>
2026-06-05 14:44:03 +0330

    update ProductController

--- admin/app/Http/Controllers/ProductController.php
+++ admin/app/Http/Controllers/ProductController.php
@@ -10,55 +10,40 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         return Product::all();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $product = Product::create($request->only(['title', 'image']));
 
-        ProductCreated::dispatch($product->toArray());
+        ProductCreated::dispatch($product->toArray())->onQueue("main_queue");
 
         return response($product);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         return Product::find($id);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         $product = Product::find($id);
         $product->update($request->only(['title', 'image']));
 
-        ProductUpdated::dispatch($product->toArray());
+        ProductUpdated::dispatch($product->toArray())->onQueue("main_queue");
 
         return response($product);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         Product::destroy($id);
 
-        ProductDeleted::dispatch($id);
+        ProductDeleted::dispatch($id)->onQueue("main_queue");
 
         return response(null);
     }
